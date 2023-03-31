1a989b531976a5a95a5bc07a7dde93fb03d4a861
Qquiqlerr <leha.metlushko@bk.ru>
2023-03-31 19:52:47 +0300

    Бот стал асинхронным

--- Bot.py
+++ Bot.py
@@ -1,18 +1,27 @@
-import telebot
-from telebot import types
+from telebot.async_telebot import AsyncTeleBot
+import asyncio
+from telebot import types, asyncio_filters
 import json
 from tables import *
 import all_funcs
 import pysondb as db
 import os
+from telebot.asyncio_storage import StateMemoryStorage
+from telebot.asyncio_handler_backends import State, StatesGroup
+
+
+class WorkStates(StatesGroup):
+    reg = State()
+    work = State()
+    per = State()
+    reply_to = State()
+    work_start = State()
+
+
 with open('token.json','r') as t:
     token = json.load(t)
-bot = telebot.TeleBot(token['token'])
-a = ''
+bot = AsyncTeleBot(token['token'],state_storage=StateMemoryStorage())
 
-type_codes = [
-    ['first_half_year', 30, ]
-]
 
 funcs = types.ReplyKeyboardMarkup()
 
@@ -44,56 +53,67 @@ for i in marks_per:
 
 
 @bot.message_handler(commands=['start'])
-def hello_and_reg(msg):
-    bot.send_message(msg.chat.id,"Привет! Давай начнем!")
+async def hello_and_reg(msg):
+    await bot.send_message(msg.chat.id,"Привет! Давай начнем!")
     if len(database.getByQuery(query={"user_id": msg.chat.id})) == 0:
-        send = bot.send_message(msg.chat.id, "Введите логин и пароль в формате\n"
-                                      "Login:pass")
-        bot.register_next_step_handler(send, reg)
+        await bot.set_state(msg.from_user.id, WorkStates.reg, msg.chat.id)
+        await bot.send_message(msg.chat.id, "Введите логин и пароль в формате\n"
+                                            "Login:pass")
+        print(await bot.get_state(msg.from_user.id, msg.chat.id))
     else:
-        reply_to(msg)
-def reg(msg):
+        await bot.set_state(msg.from_user.id, WorkStates.reply_to, msg.chat.id)
+        await reply_to(msg)
+@bot.message_handler(state= WorkStates.reg)
+async def reg(msg):
     try:
         data = all_funcs.create_data(msg.text)
         all_funcs.auth_and_create_cookies(data, msg.chat.id)
-        reply_to(msg)
+        await reply_to(msg)
     except Exception:
-        bot.send_message(msg.chat.id,"Что-то пошло не так( \n"
+        await bot.send_message(msg.chat.id,"Что-то пошло не так( \n"
                                      "Попробуйте еще раз, или обратитесь к админу")
-        hello_and_reg(msg)
-    return 0
-def reply_to(msg):
-    bot.send_message(msg.chat.id,"Отлично! Выберите одну из доступных опций",reply_markup=funcs)
+        await hello_and_reg(msg)
 
-@bot.message_handler(content_types=['text'])
-def work_start(msg):
-    bot.send_message(msg.chat.id,'Отлично! Выбери, что тебе нужно',reply_markup=marks_markup)
-    bot.register_next_step_handler(msg,work)
-def work(msg):
+
+
+
+@bot.message_handler(state=WorkStates.work)
+async def work(msg):
+    print('dgfiomvbcoi')
     if msg.text == 'Оценки за период':
-        bot.send_message(msg.chat.id, 'Отлично! Выбери период', reply_markup=marks_per_markup)
-        bot.register_next_step_handler(msg, per)
+        await bot.send_message(msg.chat.id, 'Отлично! Выбери период', reply_markup=marks_per_markup)
+        await bot.set_state(msg.from_user.id, WorkStates.per, msg.chat.id)
     elif msg.text == 'Текущие оценки':
         info = all_funcs.get_current_marks(msg.chat.id)
         for i in info:
-            bot.send_message(msg.chat.id, i)
-        bot.register_next_step_handler(msg, work)
+            await bot.send_message(msg.chat.id, i)
     elif msg.text == 'Назад':
-        reply_to(msg)
-def per(msg):
+        await reply_to(msg)
+
+@bot.message_handler(state=WorkStates.per)
+async def per(msg):
     if msg.text == 'Оценки за первое полугодие':
-        bot.send_message(msg.chat.id, all_funcs.get_marks(msg.chat.id, 'first_half'))
-        bot.register_next_step_handler(msg, per)
+        await bot.send_message(msg.chat.id, all_funcs.get_marks(msg.chat.id, 'first_half'))
     elif msg.text == 'Оценки за второе полугодие':
-        bot.send_message(msg.chat.id, all_funcs.get_marks(msg.chat.id, 'second_half'))
-        bot.register_next_step_handler(msg, per)
+        await bot.send_message(msg.chat.id, all_funcs.get_marks(msg.chat.id, 'second_half'))
     elif msg.text == 'Оценки за год':
-        bot.send_message(msg.chat.id, all_funcs.get_marks(msg.chat.id, 'year'))
-        bot.register_next_step_handler(msg, per)
+        await bot.send_message(msg.chat.id, all_funcs.get_marks(msg.chat.id, 'year'))
     elif msg.text == 'Назад':
-        work_start(msg)
+        await work_start(msg)
+
+@bot.message_handler(state= WorkStates.reply_to)
+async def reply_to(msg):
+    await bot.send_message(msg.chat.id,"Выберите одну из доступных опций",reply_markup=funcs)
+    await bot.set_state(msg.from_user.id, WorkStates.work_start, msg.chat.id)
+
+@bot.message_handler(state= WorkStates.work_start)
+async def work_start(msg):
+    await bot.send_message(msg.chat.id,'Отлично! Выбери, что тебе нужно',reply_markup=marks_markup)
+    await bot.set_state(msg.from_user.id, WorkStates.work, msg.chat.id)
 
 
 
+bot.add_custom_filter(asyncio_filters.StateFilter(bot))
+bot.add_custom_filter(asyncio_filters.IsDigitFilter())
 
-bot.infinity_polling()
+asyncio.run(bot.infinity_polling())
